c7c90ac46e5bf449ffac2c748ed00ac74868f839
NabucodonosorII <91675628+NabucodonosorII@users.noreply.github.com>
2021-12-02 08:00:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "lib.cpp"
+using namespace std;
+char a;
+int main() {
+    cin >> a;
+    char converti(a);
+    if(converti(a)=Vero){cout << a << endl;}
+    else{cout << "errore" <<endl;}
 
-int main(){
-  return 0;
+    return 0;
 }
