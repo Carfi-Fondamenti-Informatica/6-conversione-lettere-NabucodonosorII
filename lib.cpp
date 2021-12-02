1deea08b8773b171b2a4253fdcbb2cf1e26aff7a
NabucodonosorII <91675628+NabucodonosorII@users.noreply.github.com>
2021-12-02 07:59:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,7 @@
+#include "lib.h"
 
+char converti (char &carattere) {char Vero = Vero; char False=False;
+    if(carattere >='A' && carattere <='Z'){char minuscolo = carattere +32;return Vero;}
+    else if(carattere >='a' && carattere <='z'){char maiuscolo = carattere -32;return Vero;}
+    else {return False;};
+}
